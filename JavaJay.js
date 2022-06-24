d6254004912abf3db471f37a9db162807f88c60e
Adehi Joshua <adehijoshua@gmail.com>
2022-06-24 16:39:18 +0100

    first commit

---
+++ JavaJay.js
@@ -0,0 +1,5 @@
+let myName = "Adehi Joshua";
+let myHeight = 5.8;
+let myCountry = "Nigeria";
+
+document.write("My name is "+myName +  "," + " " + "my height is barely "+myHeight +"ft and" +" " +"my country is "+myCountry +" ")
